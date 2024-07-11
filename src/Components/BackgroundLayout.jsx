448a6dbfa6ae3c17d7f7ca0022cbf5b2f753632d
Manikanta416 <manikanta2096@example.com>
2024-07-11 14:41:19 +0530

    hhh

--- src/Components/BackgroundLayout.jsx
+++ src/Components/BackgroundLayout.jsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import { useStateContext } from '../Context'
-//images
-import Clear from '../assets/images/Clear.jpg'
-import Fog from '../assets/images/fog.png'
-import Cloudy from '../assets/images/Cloudy.jpg'
-import Rainy from '../assets/images/Rainy.jpg'
-import Snow from '../assets/images/snow.jpg'
-import Stormy from '../assets/images/Stormy.jpg'
+import React, { useEffect, useState } from 'react';
+import { useStateContext } from '../Context';
+// images
+import Clear from '../assets/images/Clear.jpg';
+import Fog from '../assets/images/fog.png';
+import Cloudy from '../assets/images/Cloudy.jpg';
+import Rainy from '../assets/images/Rainy.jpg';
+import Snow from '../assets/images/snow.jpg';
+import Stormy from '../assets/images/Stormy.jpg';
 
-
-  const { weather } = useStateContext()
-  const [image, setImage] = useState(Clear)
+const BackgroundLayout = () => {
+  const { weather } = useStateContext();
+  const [image, setImage] = useState(Clear);
 
   useEffect(() => {
-    if (weather.conditions) {
-      let imageString = weather.conditions
+    if (weather?.conditions) {
+      let imageString = weather.conditions;
       if (imageString.toLowerCase().includes('clear')) {
-        setImage(Clear)
+        setImage(Clear);
       } else if (imageString.toLowerCase().includes('cloud')) {
-        setImage(Cloudy)
+        setImage(Cloudy);
       } else if (imageString.toLowerCase().includes('rain') || imageString.toLowerCase().includes('shower')) {
-        setImage(Rainy)
+        setImage(Rainy);
       } else if (imageString.toLowerCase().includes('snow')) {
-        setImage(Snow)
+        setImage(Snow);
       } else if (imageString.toLowerCase().includes('fog')) {
-        setImage(Fog)
+        setImage(Fog);
       } else if (imageString.toLowerCase().includes('thunder') || imageString.toLowerCase().includes('storm')) {
-        setImage(Stormy)
+        setImage(Stormy);
       }
     }
-  }, [weather])
+  }, [weather]);
 
   return (
-    <img src={image} alt="weather_image" className='h-screen w-full fixed left-0 top-0 -z-[10]' />
-  )
-}
+    <img src={image} alt="weather_image" className="h-screen w-full fixed left-0 top-0 -z-[10]" />
+  );
+};
 
-export default BackgroundLayout
+export default BackgroundLayout;
